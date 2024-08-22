a7cf2ad0277b8d60c39845346340972bdb0e37a5
kittitnun <kittitnun@gmail.com>
2024-08-22 16:13:50 +0700

    update

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// import (
+// 	"net/http"
+
+// 	"github.com/labstack/echo/v4"
+// )
+
+// func main() {
+// 	e := echo.New()
+// 	e.GET("/", func(c echo.Context) error {
+// 		return c.String(http.StatusOK, "Hello, World!")
+// 	})
+// 	e.Logger.Fatal(e.Start(":1323"))
+// }
+
 func main() {
-	fmt.Println("hello world")
+	//implicit type
+	var a int = 10
+	fmt.Println(a)
+	//Dynamisc type
+	b := 20
+	fmt.Println(b)
+	c := 2.2
+	fmt.Println(c)
 }
